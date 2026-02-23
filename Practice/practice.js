e502b311f076172cd3e13671db356681372a7bac
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-23 10:05:40 +0100

    new

--- Practice/practice.js
+++ Practice/practice.js
@@ -50,6 +50,7 @@ for (var i = 0; i < 100; i++) {
 }
 */
 
+/*
 var output = [];
 var count = 1;
 
@@ -68,7 +69,28 @@ function fizzBuzz(){
   }
   console.log(output);
 }
+*/
 
+ function fibonacciGenerator(n) {
+
+    var output = [];
+    
+    if (n === 1) {
+        output = [0];
+    } else if (n === 2) {
+        output = [0, 1];
+    } else {
+        output = [0, 1];
+        
+        for (var i = 2; i < n; i++) {
+            output.push(output[i - 2] + output[i - 1]);
+        }
+    }
+    
+    return output;
+}
+fibonacciGenerator(5);
+console.log(output);
 
 
 
