b455b3d415b58d91679df7da2a0d62802008bb7c
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-27 10:21:21 +0100

    change

--- C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709f045d74c8b27b7918896bb1d2a2730aedd5cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc9c2fea8e10df9c7e926f0553db13257194100")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
